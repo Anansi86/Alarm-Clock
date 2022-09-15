8471c9236fceb2c5bcf249d07400504aebd266ba
Anansi86 <shawn.staton@student.awesomeinc.org>
2022-09-15 16:14:28 +0000

    setInterval

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
     function clock() {
       var clock = new Date();
+      var hour = clock.getHours();
+      var min = clock.getMinutes();
+      var sec = clock.getSeconds();
+      var month = clock.getMonth();
+      
+      var liveTime = hour + ":" + min + ":" + sec 
     
-        document.getElementById("clock").innerHTML = clock;
     
+    
+    
+       document.getElementById("clock").innerHTML = liveTime;
+        console.log(clock);
+        
     }
     
-    setTimeout(clock, 1000);
-    console.log(clock);
-
-    
+   setInterval(clock, 1000);
+  
