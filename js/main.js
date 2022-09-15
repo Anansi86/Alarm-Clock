5a10e45e5736baa2c5c0395aa5c7fd52d65c36c6
Anansi86 <shawn.staton@student.awesomeinc.org>
2022-09-15 16:28:12 +0000

    month day year

--- js/main.js
+++ js/main.js
@@ -3,11 +3,14 @@
       var hour = clock.getHours();
       var min = clock.getMinutes();
       var sec = clock.getSeconds();
-      var month = clock.getMonth();
-      
-      var liveTime = hour + ":" + min + ":" + sec 
+      var month = clock.getMonth() + 1;
+      var day = clock.getDay();
+      var year = clock.getFullYear();
+
+
+      var liveTime = hour + ":" + min + ":" + sec + " Month - " + month + "/" + day + "/" + year;
     
-    
+        
     
     
        document.getElementById("clock").innerHTML = liveTime;
