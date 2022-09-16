15e5afa85e28742a5eeee5d4bd2d852e438c58c4
Anansi86 <shawn.staton@student.awesomeinc.org>
2022-09-16 05:21:18 +0000

    made alarm box

--- js/main.js
+++ js/main.js
@@ -12,18 +12,7 @@
       var date = "Date: " + month + "/" + day + "/" + year;
         
       
-      
-//        let alarm = prompt("Set Alarm", "");
-  //      if (alarm == null || alarm == "") {
-    //      text = "User cancelled the prompt.";
-      //  } else {
-        //  text = "Hello " + alarm + "! How are you today?";
-        //}
-       // document.getElementById("alarm").innerHTML = text;
-      
-        
-    // if (document.getElementByid("alarm") === liveTime)
-       
+ 
        document.getElementById("clock").innerHTML = liveTime;
        document.getElementById("date").innerHTML = date;
        
